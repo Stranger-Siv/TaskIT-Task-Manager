bab9e8bf62f08c54d329754056f4c90150ddf1c6
Sivram Pradhan <programmersiv21@gmail.com>
2025-02-05 14:10:43 +0530

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -11,6 +11,23 @@ const app = express()
 const PORT = process.env.PORT;
 app.use(express.json());
 
+const url = `https://taskit-task-manager.onrender.com`;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+
 app.use(cors());  
 app.use('/api/task', taskRoutes)
 
